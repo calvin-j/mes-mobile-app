85577f055b11856584b49a116f543362cc9befdc
Brian Simpson <brian@inhand.co.uk>
2018-12-10 09:10:58 +0000

    Added npm scripts for configuration of environments

---
+++ config/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  isRemote: false,
+  remoteSettingsUrl: 'dev',
+  googleAnalyticsId: 'UA-129814222-1',
+  userIdDimensionIndex: 1,
+  journalApiUrl: 'assets/data/journalResp.json'
+};
---
+++ config/environment.uat.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  isRemote: true,
+  remoteSettingsUrl: 'assets/config/environmentUat.json',
+  googleAnalyticsId: 'UA-129814222-1',
+  userIdDimensionIndex: 1,
+  journalApiUrl: 'assets/data/journalResp.json'
+};
